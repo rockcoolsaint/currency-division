bcb41245c815f690e68471b6ccad06268ad82c54
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2022-04-08 16:50:37 +0100

    added more currencies and dynamic divisions

--- src/App.js
+++ src/App.js
@@ -4,33 +4,38 @@ import './App.css';
 function App() {
   const amountRef = useRef(null);
 
-  const [cent, setCent] = useState("");
+  const [division, setDivision] = useState("");
   const [currency, setCurrency] = useState("dollar");
 
-  const inputValidation = (inputString, currency) => {
-    if (currency === "dollar" && inputString.match(/(?=.*?\d)^\$?(([1-9]\d{0,2}(,\d{3})*)|\d+)?(\.\d{1,2})?$/) === null) {
-      alert(`invalid Input, please type a valid ${currency} amount`);
-      return false;
-    } else if (currency === "euro" && inputString.match(/(?=.*?\d)^\$?(([1-9]\d{0,2}(.\d{3})*)|\d+)?(,\d{1,2})?$/) === null) {
-      alert(`invalid Input, please type a valid ${currency} amount`);
-      return false;
-    }
+  const currencyDivision = {
+    dollar: "Cents",
+    euro: "Cents",
+    inr: "Paisa",
+    naira: "Kobo"
   }
   
   const handleFormSubmit = (e) => {
     e.preventDefault();
 
-    // amount validation
-    // validation for dollar and euro
-    let val = inputValidation(amountRef?.current?.value, currency);
-
-    if (val !== false) {
-      console.log(`val: ${val}`)
-      if (currency === "dollar") {
-        setCent(amountRef?.current?.value * 100);
-      } else {
-        setCent(amountRef?.current?.value.replace(/[.,]/g, ''));
-      }
+    switch (currency) {
+      case "dollar":
+      case "naira":
+      case "inr":
+        if (amountRef?.current?.value.match(/(?=.*?\d)^\$?(([1-9]\d{0,2}(,\d{3})*)|\d+)?(\.\d{1,2})?$/) === null) {
+          alert(`invalid Input, please type a valid ${currency.toUpperCase()} amount`);
+        } else {
+          setDivision(amountRef?.current?.value * 100);
+        }
+        break;
+      case "euro":
+        if (amountRef?.current?.value.match(/(?=.*?\d)^\$?(([1-9]\d{0,2}(.\d{3})*)|\d+)?(,\d{1,2})?$/) === null) {
+          alert(`invalid Input, please type a valid ${currency.toUpperCase()} amount`);
+        } else {
+          setDivision(amountRef?.current?.value.replace(/[.,]/g, ''));
+        }
+        break;
+      default:
+        break;
     }
   }
 
@@ -47,12 +52,14 @@ function App() {
           <select name="currency" value={currency} onChange={(e) => setCurrency(e.target.value)}>
             <option value="dollar">Dollar</option>
             <option value="euro">Euro</option>
+            <option value="naira">Naira</option>
+            <option value="inr">INR</option>
           </select>
         </label>
         <br />
         <label>
-          Cents: &nbsp;
-          <input value={cent} />
+          {currencyDivision[currency]}: &nbsp;
+          <input value={division} />
         </label>
         <br />
         <input type="submit" value="submit" />
