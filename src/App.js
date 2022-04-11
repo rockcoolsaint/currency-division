a37cef4e35306688dafb73a3931f7323d66402e9
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2022-04-11 22:32:03 +0100

    Added JPY and more currencies

--- src/App.js
+++ src/App.js
@@ -11,7 +11,19 @@ function App() {
     dollar: "Cents",
     euro: "Cents",
     inr: "Paisa",
-    naira: "Kobo"
+    naira: "Kobo",
+    jpy: "Yen",
+    cve: "Escudo",
+    djf: "Franc",
+    idr: "Rupiah",
+    kmf: "Franc",
+    krw: "Won",
+    pyg: "Guarani",
+    rwf: "Franc",
+    bhd: "Fils",
+    iqd: "Fils",
+    jod: "Fulus",
+    kwd: "Fils"
   }
   
   const handleFormSubmit = (e) => {
@@ -24,7 +36,30 @@ function App() {
         if (amountRef?.current?.value.match(/(?=.*?\d)^\$?(([1-9]\d{0,2}(,\d{3})*)|\d+)?(\.\d{1,2})?$/) === null) {
           alert(`invalid Input, please type a valid ${currency.toUpperCase()} amount`);
         } else {
-          setDivision(amountRef?.current?.value * 100);
+          setDivision(amountRef?.current?.value.replace(/,/g, '') * 100);
+        }
+        break;
+      case "bhd":
+      case "iqd":
+      case "jod":
+      case "kwd":
+        if (amountRef?.current?.value.match(/(?=.*?\d)^\$?(([1-9]\d{0,2}(,\d{3})*)|\d+)?(\.\d{1,2})?$/) === null) {
+          alert(`invalid Input, please type a valid ${currency.toUpperCase()} amount`);
+        } else {
+          setDivision(amountRef?.current?.value.replace(/,/g, '') * 1000);
+        }
+        break;
+      case "jpy":
+      case "cve":
+      case "djf":
+      case "idr":
+      case "kmf":
+      case "pyg":
+      case "rwf":
+        if (amountRef?.current?.value.match(/(?=.*?\d)^\$?(([1-9]\d{0,2}(,\d{3})*)|\d+)?$/) === null) {
+          alert(`invalid Input, please type a valid ${currency.toUpperCase()} amount`);
+        } else {
+          setDivision(amountRef?.current?.value);
         }
         break;
       case "euro":
@@ -54,6 +89,17 @@ function App() {
             <option value="euro">Euro</option>
             <option value="naira">Naira</option>
             <option value="inr">INR</option>
+            <option value="jpy">JPY</option>
+            <option value="cve">Cape Verdi Escudo</option>
+            <option value="djf">Djibouti Franc</option>
+            <option value="idr">Indonesian Rupiah</option>
+            <option value="kmf">Comoro Franc</option>
+            <option value="pyg">Paraguay Guarani</option>
+            <option value="rwf">Rwanda Franc</option>
+            <option value="bhd">Bahraini Dinar</option>
+            <option value="iqd">Iraqi Dinar</option>
+            <option value="jod">Jordanian Dinar</option>
+            <option value="kwd">Kuwaiti Dinar</option>
           </select>
         </label>
         <br />
